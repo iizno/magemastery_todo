5ff5ca1f0a071db06f1288e0b06b39a1854b2419
iizno <j.ansel@cld.be>
2021-05-24 14:11:58 +0200

    lesson 21

--- app/code/MageMastery/Todo/view/frontend/web/js/view/todo.js
+++ app/code/MageMastery/Todo/view/frontend/web/js/view/todo.js
@@ -1,26 +1,31 @@
-define(['uiComponent'], function(Component) {
+ define(['uiComponent', 'jquery'], function(Component, $) {
     'use strict';
     return Component.extend({
         defaults: {
             tasks: [
-                {label: "Task 1"},
-                {label: "Task 2"},
-                {label: "Task 3"},
-                {label: "Task 4"},
+                {id: 1, label: "Task 1", status: false},
+                {id: 2, label: "Task 2", status: false},
+                {id: 3, label: "Task 3", status: false},
+                {id: 4, label: "Task 4", status: true},
             ]
         },
         initObservable: function() {
             this._super().observe([
                 'tasks'
             ]);
-
-            this.tasks.push(
-                {
-                    label: "Task 5"
-                }
-            );
-
             return this;
+        },
+        switchStatus: function (data, event) {
+            const task_id = $(event.target).data('id');
+            var items = this.tasks().map(function(task) {
+                if(task.id === task_id) {
+                    task.status = !task.status;
+                }
+
+                return task;
+            });
+
+            return this.tasks(items);
         }
     });
 });
